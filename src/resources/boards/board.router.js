1edb7211740c44f53b803ed222b833fbb9ea8b51
Valery Lutsevich <tsubasa@tut.by>
2020-10-19 14:04:35 +0300

    feat: catch errors for board and task

--- src/resources/boards/board.router.js
+++ src/resources/boards/board.router.js
@@ -1,44 +1,55 @@
 const router = require('express').Router();
 const Board = require('./board.model');
 const boardsService = require('./board.service');
+const { catchErrors } = require('../../common/errors');
 
-router.route('/').get(async (req, res) => {
-  const boards = await boardsService.getAll();
-  res.json(boards);
-});
+router.route('/').get(
+  catchErrors(async (req, res) => {
+    const boards = await boardsService.getAll();
+    res.json(boards);
+  })
+);
 
-router.route('/:id').get(async (req, res) => {
-  try {
-    const board = await boardsService.get(req.params.id);
+router.route('/:id').get(
+  catchErrors(async (req, res) => {
+    try {
+      const board = await boardsService.get(req.params.id);
+      res.json(board);
+    } catch (e) {
+      res.status(404).send(e.message);
+    }
+  })
+);
+
+router.route('/').post(
+  catchErrors(async (req, res) => {
+    const board = await boardsService.create(new Board(req.body));
     res.json(board);
-  } catch (e) {
-    res.status(404).send(e.message);
-  }
-});
+  })
+);
 
-router.route('/').post(async (req, res) => {
-  const board = await boardsService.create(new Board(req.body));
-  res.json(board);
-});
+router.route('/:id').put(
+  catchErrors(async (req, res) => {
+    try {
+      const board = await boardsService.update(
+        new Board({ ...req.body, id: req.params.id })
+      );
+      res.json(board);
+    } catch (e) {
+      res.status(404).send(e.message);
+    }
+  })
+);
 
-router.route('/:id').put(async (req, res) => {
-  try {
-    const board = await boardsService.update(
-      new Board({ ...req.body, id: req.params.id })
-    );
-    res.json(board);
-  } catch (e) {
-    res.status(404).send(e.message);
-  }
-});
-
-router.route('/:id').delete(async (req, res) => {
-  try {
-    await boardsService.remove(req.params.id);
-    res.json({});
-  } catch (e) {
-    res.status(404).send(e.message);
-  }
-});
+router.route('/:id').delete(
+  catchErrors(async (req, res) => {
+    try {
+      await boardsService.remove(req.params.id);
+      res.json({});
+    } catch (e) {
+      res.status(404).send(e.message);
+    }
+  })
+);
 
 module.exports = router;
